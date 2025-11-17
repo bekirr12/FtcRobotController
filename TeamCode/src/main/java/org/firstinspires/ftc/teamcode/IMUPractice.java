5cd4373f874433514bd9b66e28704a6817bee67e
bekirr12 <bekirsamikaratas@gmail.com>
2025-11-17 12:43:06 +0300

    Create IMU example

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IMUPractice.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
+
+@TeleOp
+public class IMUPractice extends OpMode {
+    TestIMU bench = new TestIMU();
+
+
+    @Override
+    public void init() {
+        bench.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        telemetry.addData("Heading", bench.getHeading(AngleUnit.DEGREES));
+    }
+}
